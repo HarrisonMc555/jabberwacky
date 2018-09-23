cfcb4f10ecc2feeb81ee413720a2582275c069b3
Harrison McCullough <mccullough.harrison@gmail.com>
2018-09-23 16:54:44 -0600

    Attempt to process audio manually (untested)

--- js/microphone.js
+++ js/microphone.js
@@ -1,8 +1,32 @@
-var recorder = document.getElementById('recorder');
+/* var recorder = document.getElementById('recorder'); */
 var player = document.getElementById('player');
 
-recorder.addEventListener('change', function(e) {
-  var file = e.target.files[0];
-  // Do something with audio file.
-  player.src = URL.createObjectURL(file);
-});
+/* recorder.addEventListener('change', function(e) {
+ *   var file = e.target.files[0];
+ *   // Do something with audio file.
+ *   player.src = URL.createObjectURL(file);
+ * });
+ *  */
+
+var handleSuccess = function(stream) {
+  var context = new AudioContext();
+  var source = context.createMediaStreamSource(stream);
+  var processor = context.createScriptProcessor(1024, 1, 1);
+
+  source.connect(processor);
+  processor.connect(context.destination);
+
+  processor.onaudioprocess = function(e) {
+    // do something with the data
+    console.log(e.inputBuffer);
+  }
+
+  /* if (window.URL) {
+   *   player.src = window.URL.createObjectURL(stream);
+   * } else {
+   *   player.src = stream;
+   * } */
+};
+
+navigator.mediaDevices.getUserMedia({ audio: true, video: false})
+         .then(handleSuccess);
